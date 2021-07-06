1a1639e166b702d7e7b9299ef4c88cee00184abd
marwaha-raghav <marwaha.raghav@rediffmail.com>
2021-07-06 16:11:30 -0400

    added cs Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Starter;
+
+namespace test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Starter.Program ob = new Starter.Program();
+            game gObject = new game();
+            ob.func();
+            gObject.GameLogic();
+        }
+    }
+}
