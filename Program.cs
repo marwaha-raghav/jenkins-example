919f776cbdfc7041cb9d163fdb4ea09f82ab477b
marwaha-raghav <marwaha.raghav@rediffmail.com>
2021-08-02 16:00:40 -0400

    test commit for hook

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace test_project
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            //test comment for git
             Console.WriteLine("This is a friendly C# Program");
             Console.WriteLine("Continue");
             Starter.Program ob = new Starter.Program();
